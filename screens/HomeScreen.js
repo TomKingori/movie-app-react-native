83364e7ea1ff179425c97546cc88e05963cd1c17
TomKingori <tomkingori66@gmail.com>
2023-11-21 16:46:52 +0300

    home nav UI

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { styles } from "../theme";
+
+const ios = Platform.OS == "ios";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 bg-neutral-800">
-      <Text>HomeScreen</Text>
+      {/* search bar and logo */}
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
+          <View className="flex-row justify-between items-center mx-4">
+            <Bars3CenterLeftIcon size={30} strokeWidth={2} color="white" />
+            <Text className="text-white text-3xl font-bold">
+              <Text style={styles.text}>TKM</Text>ovies
+            </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size={30} strokeWidth={2} color="white"/>
+          </TouchableOpacity>
+          </View>
+      </SafeAreaView>
     </View>
   );
 }
