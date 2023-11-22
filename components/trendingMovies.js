5b6e4edc887ae96b922503620c1aef87457363be
TomKingori <tomkingori66@gmail.com>
2023-11-22 12:50:14 +0300

    trending movies card

---
+++ components/trendingMovies.js
@@ -0,0 +1,33 @@
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import React from "react";
+import Carousel from "react-native-snap-carousel";
+
+export default function TrendingMovies({ data }) {
+  return (
+    <View className="mb-8">
+      <Text className="text-white text-xl mx-4 mb-5">Trending</Text>
+      <Carousel
+        data={data}
+        renderItem={({ item }) => <MovieCard item={item} />}
+        firstItem={1}
+        inactiveSlideOpacity={0.6}
+        sliderWidth={600}
+        itemWidth={400}
+        slideStyle={{ display: "flex", alignItems: "center" }}
+      ></Carousel>
+    </View>
+  );
+}
+
+const MovieCard = ({ item }) => {
+  return (
+    <TouchableWithoutFeedback>
+      <Text>Movie</Text>
+    </TouchableWithoutFeedback>
+  );
+};
