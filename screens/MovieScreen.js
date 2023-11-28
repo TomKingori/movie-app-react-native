3322a59f8d945c805011f05aac75f74e85bd213b
TomKingori <tomkingori66@gmail.com>
2023-11-28 16:17:07 +0300

    moviescreen UI

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -5,12 +5,14 @@ import {
   ScrollView,
   TouchableOpacity,
   Platform,
+  Image
 } from "react-native";
-import React, { useEffect } from "react";
-import { useRoute } from "@react-navigation/native";
+import React, { useEffect, useState } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { ChevronLeftIcon, HeartIcon } from "react-native-heroicons/outline";
-import { styles } from "../theme";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles, theme } from "../theme";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
@@ -18,6 +20,8 @@ const topMargin = ios ? "": " mt-3";
 
 export default function MovieScreen() {
   const { params: item } = useRoute();
+  const [isFavourite, toggleFavourite] = useState(false);
+  const navigation = useNavigation();
   useEffect(() => {
     //fetch movie details using API
   }, [item]);
@@ -30,15 +34,22 @@ export default function MovieScreen() {
       <View className="w-full">
         <SafeAreaView className={"absolite z-20 w-full flex-row justify-between items-center px-4"+topMargin}>
           <TouchableOpacity
+          onPress={()=> navigation.goBack()}
             style={styles.background}
             className="rounded-xl p-1"
           >
             <ChevronLeftIcon size="25" strokeWidth={2.5} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity>
-            <HeartIcon size="35" color="white" />
+          <TouchableOpacity onPress={()=> toggleFavourite(!isFavourite)}>
+            <HeartIcon size="35" color={isFavourite? theme.background : "white"} />
           </TouchableOpacity>
         </SafeAreaView>
+
+        <View>
+          <Image source={require('../assets/images/moviePoster.png')}
+          style={{width, height: height*0.55}}
+           />
+        </View>
       </View>
     </ScrollView>
   );
