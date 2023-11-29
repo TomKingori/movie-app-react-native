d6b28f4ca8d7781a47b099e0f69da3a0b26b4df0
TomKingori <tomkingori66@gmail.com>
2023-11-29 12:58:57 +0300

    movie poster

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -5,7 +5,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Platform,
-  Image
+  Image,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
@@ -13,10 +13,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ChevronLeftIcon } from "react-native-heroicons/outline";
 import { HeartIcon } from "react-native-heroicons/solid";
 import { styles, theme } from "../theme";
+import { LinearGradient } from "expo-linear-gradient";
 
 var { width, height } = Dimensions.get("window");
 const ios = Platform.OS == "ios";
-const topMargin = ios ? "": " mt-3";
+const topMargin = ios ? "" : " mt-3";
 
 export default function MovieScreen() {
   const { params: item } = useRoute();
@@ -32,25 +33,43 @@ export default function MovieScreen() {
     >
       {/* Back button and movie poster */}
       <View className="w-full">
-        <SafeAreaView className={"absolite z-20 w-full flex-row justify-between items-center px-4"+topMargin}>
+        <SafeAreaView
+          className={
+            "absolite z-20 w-full flex-row justify-between items-center px-4" +
+            topMargin
+          }
+        >
           <TouchableOpacity
-          onPress={()=> navigation.goBack()}
+            onPress={() => navigation.goBack()}
             style={styles.background}
             className="rounded-xl p-1"
           >
             <ChevronLeftIcon size="25" strokeWidth={2.5} color="white" />
           </TouchableOpacity>
-          <TouchableOpacity onPress={()=> toggleFavourite(!isFavourite)}>
-            <HeartIcon size="35" color={isFavourite? theme.background : "white"} />
+          <TouchableOpacity onPress={() => toggleFavourite(!isFavourite)}>
+            <HeartIcon
+              size="35"
+              color={isFavourite ? theme.background : "white"}
+            />
           </TouchableOpacity>
         </SafeAreaView>
 
         <View>
-          <Image source={require('../assets/images/moviePoster.png')}
-          style={{width, height: height*0.55}}
-           />
+          <Image
+            source={require("../assets/images/moviePoster.png")}
+            style={{ width, height: height * 0.55 }}
+          />
+          <LinearGradient
+            colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
+            style={{width, height: height*0.40}}
+            start={{x: 0.5, y:0}}
+            end={{x: 0.5, y:1}}
+            className="absolute bottom-0"
+          />
         </View>
       </View>
+
+      {/* Movie details */}
     </ScrollView>
   );
 }
