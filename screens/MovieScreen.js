86e442fc8d3ce7c360a11ae224575e98c5519f28
TomKingori <tomkingori66@gmail.com>
2023-11-29 13:27:55 +0300

    moviescreen UI

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -23,6 +23,7 @@ export default function MovieScreen() {
   const { params: item } = useRoute();
   const [isFavourite, toggleFavourite] = useState(false);
   const navigation = useNavigation();
+  let movieName = "Inception";
   useEffect(() => {
     //fetch movie details using API
   }, [item]);
@@ -60,16 +61,45 @@ export default function MovieScreen() {
             style={{ width, height: height * 0.55 }}
           />
           <LinearGradient
-            colors={['transparent', 'rgba(23,23,23,0.8)', 'rgba(23,23,23,1)']}
-            style={{width, height: height*0.40}}
-            start={{x: 0.5, y:0}}
-            end={{x: 0.5, y:1}}
+            colors={["transparent", "rgba(23,23,23,0.8)", "rgba(23,23,23,1)"]}
+            style={{ width, height: height * 0.4 }}
+            start={{ x: 0.5, y: 0 }}
+            end={{ x: 0.5, y: 1 }}
             className="absolute bottom-0"
           />
         </View>
       </View>
 
       {/* Movie details */}
+      <View style={{ marginTop: -(height * 0.09) }} className="space-y-3">
+        <Text className="text-white text-center text-3xl font-bold tracking-wider">
+          {movieName}
+        </Text>
+        {/* status, release and runtime */}
+        <Text className="text-neutral-400 font-semibold text-base text-center">
+          Released • 2014 • 170 min
+        </Text>
+        {/* Genres */}
+        <View className="flex-row justify-center mx-4 space-x-2">
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Action •
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Thrill •
+          </Text>
+          <Text className="text-neutral-400 font-semibold text-base text-center">
+            Comedy
+          </Text>
+        </View>
+
+        {/* Description */}
+        <Text className="text-neutral-400 mx-4 tracking-wide">
+          Inception centres on brooding “extractor” Dom Cobb (played by Leonardo
+          DiCaprio)—a thief who invades targets' dreams through a
+          chemical-induced shared dream state in order to steal valuable
+          information.
+        </Text>
+      </View>
     </ScrollView>
   );
 }
